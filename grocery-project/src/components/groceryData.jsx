596ba5b2f3daa4320c6918197f2d52ac9eda9a66
alexdavies440 <alexdavies440@gmail.com>
2024-09-20 23:07:30 -0500

    checkpoint

--- grocery-project/src/components/groceryData.jsx
+++ grocery-project/src/components/groceryData.jsx
@@ -1,55 +1,46 @@
-class groceryItem {
-    constructor(id, name, price, category, taxRate) {
-        this.name = name;
-        this.price = price;
-        this.category = category;
-        this.taxRate = taxRate;
-    }
-}
+// class groceryItem {
+//     constructor(id, name, price, category, taxRate) {
+//         this.name = name;
+//         this.price = price;
+//         this.category = category;
+//         this.taxRate = taxRate;
+//     }
+// }
 
 const bTaxable = 6.391;
 const cTaxable = 9.679;
 
 
-const apples = new groceryItem(1, "Apples", 3.69, "Fruit", bTaxable);
-const strawberries = new groceryItem(2, "Strawberries", 2.25, "Fruit", bTaxable);
-const  raspberries = new groceryItem(3, "Raspberries", 2.59, "Fruit", bTaxable);
-const blackberries = new groceryItem(4, "Blackberries", 2.29, "Fruit", bTaxable);
-const redGrapes = new groceryItem(5, "Red Grapes", 4.30, "Fruit", bTaxable);
-
-const onionBagYellow = new groceryItem("Bag of Yellow Onions", 3.19, "Vegetables", bTaxable);
-const sweetPotatoBag = new groceryItem("Sweet Potatoe Bag", 2.55, "Vegetables", bTaxable);
-const russetPotatoBag = new groceryItem("Russet Potato Bag", 4.65, "Vegetables", bTaxable);
-
-const oatBranBread = new groceryItem("Oat Bran Bread", 2.19, "Dry Goods", bTaxable);
-const ketoBread = new groceryItem("Keto Bread", 3.99, "Dry Goods", bTaxable);
-const peanutbutterSWCookies = new groceryItem("Peanutbutter Sandwich Cookies", 2.75, "Dry Goods", bTaxable);
-const kettleChips = new groceryItem("Kettle Chips", 1.95, "Dry Goods", bTaxable);
-const coffee = new groceryItem("Coffee", 4.39, "Dry Goods", bTaxable);
-
-const slicedSalami = new groceryItem("Sliced Salami", 3.09, "Deli", bTaxable);
-const slicedCheddar = new groceryItem("Sliced Cheddar", 1.65, "Deli", bTaxable);
-
-const blueMilkGal = new groceryItem("2% Milk Gallon", 3.16, "Dairy", bTaxable);
-const skimMilkHalf = new groceryItem("Skim Milk 1/2 Gallon", 1.79, "Dairy", bTaxable);
-const halfAndHalf = new groceryItem("Half & Half", 2.79, "Dairy", bTaxable);
-const soutCream = new groceryItem("Sour Cream", 1.89, "Dairy", bTaxable);
-
-const  lightOrangeJuice = new groceryItem("Light Orange Juice", 3.29, "Beverages", bTaxable);
-const sparklingWater = new groceryItem("Flavored Sparkling Water", 0.69, "Beverages", bTaxable);
-const celsius = new groceryItem("Celsius Energy Drink", 2.09, "Beverages", bTaxable);
-const sugarFreeEnergy = new groceryItem("Sugar-Free Energy Drink", 3.99, "Beverages", bTaxable);
-
-const porkSpareRibs = new groceryItem("Pork Spare Ribs", 7.16, "Meat", bTaxable);
-const groundTurkey = new groceryItem("Ground Turkey", 8.79, "Meat", bTaxable);
-
-const hashBrownPatties = new groceryItem("Hash Brown Patties", 4.95, "Freezer", bTaxable);
-const frozenPeas = new groceryItem("Frozen Peas", 1.05, "Freezer", bTaxable);
-const frozenCorn = new groceryItem("Frozen Corn", 1.05, "Freezer", bTaxable);
-const frozenMixedVeg = new groceryItem("Frozen Mixed Vegetables", 1.05, "Freezer", bTaxable);
-
-
-
-const groceryData = [apples, strawberries, raspberries, blackberries, redGrapes, onionBagYellow, sweetPotatoBag, russetPotatoBag, oatBranBread, ketoBread, kettleChips, peanutbutterSWCookies, coffee, slicedSalami, slicedCheddar, ];
+const groceryData = [
+    {id: 1, name: "Apples", price: 3.69, category: "Fruit",taxRate: bTaxable},
+    {id: 2, name: "Strawberries", price: 2.25, category: "Fruit", taxRate: bTaxable},
+    {id: 3, name: "Raspberries", price: 2.59, category: "Fruit", taxRate: bTaxable},
+    {id: 4, name: "Blackberries", price: 2.29, category: "Fruit", taxRate: bTaxable},
+    {id: 5, name: "Red Grapes", price: 4.30, category: "Fruit",taxRate: bTaxable},
+    {id: 6, name: "Bag of Yellow Onions", price: 3.19, category: "Vegetables", taxRate: bTaxable},
+    {id: 7, name: "Sweet Potatoe Bag", price: 2.55, category: "Vegetables", taxRate: bTaxable},
+    {id: 8, name: "Russet Potato Bag", price: 4.65, category: "Vegetables", taxRate: bTaxable},
+    {id: 9, name: "Oat Bran Bread", price: 2.19, category: "Dry Goods", taxRate: bTaxable},
+    {id: 10, name: "Keto Bread", price: 3.99, category: "Dry Goods", taxRate: bTaxable},
+    {id: 11, name: "Peanutbutter Sandwich Cookies", price: 2.75, category: "Dry Goods", taxRate: bTaxable},
+    {id: 12, name: "Kettle Chips", price: 1.95, category: "Dry Goods", taxRate: bTaxable},
+    {id: 13, name: "Coffee", price: 4.39, category: "Dry Goods", taxRate: bTaxable},
+    {id: 14, name: "Sliced Salami", price: 3.09, category: "Deli", taxRate: bTaxable},
+    {id: 15, name: "Sliced Cheddar", price: 1.65, category: "Deli", taxRate: bTaxable},
+    {id: 16, name: "2% Milk Gallon", price: 3.16, category: "Dairy", taxRate: bTaxable},
+    {id: 17, name: "Skim Milk 1/2 Gallon", price: 1.79, category: "Dairy", taxRate: bTaxable},
+    {id: 18, name: "Half & Half", price: 2.79, category: "Dairy", taxRate: bTaxable},
+    {id: 19, name: "Sour Cream", price: 1.89, category: "Dairy", taxRate: bTaxable},
+    {id: 20, name: "Light Orange Juice", price: 3.29, category: "Beverages", taxRate: bTaxable},
+    {id: 21, name: "Flavored Sparkling Water", price: 0.69, category: "Beverages", taxRate: bTaxable},
+    {id: 22, name: "Celsius Energy Drink", price: 2.09, category: "Beverages", taxRate: bTaxable},
+    {id: 23, name: "Sugar-Free Energy Drink", price: 3.99, category: "Beverages", taxRate: bTaxable},
+    {id: 24, name: "Pork Spare Ribs", price: 7.16, category: "Meat", taxRate: bTaxable},
+    {id: 25, name: "Ground Turkey", price: 8.79, category: "Meat", taxRate: bTaxable},
+    {id: 26, name: "Hash Brown Patties", price: 4.95, category: "Freezer", taxRate: bTaxable},
+    //{"Frozen Peas", 1.05, "Freezer", bTaxable},
+    //{"Frozen Corn", 1.05, "Freezer", bTaxable},
+    //{"Frozen Mixed Vegetables", 1.05, "Freezer", bTaxable}
+];
 
 export default groceryData;
