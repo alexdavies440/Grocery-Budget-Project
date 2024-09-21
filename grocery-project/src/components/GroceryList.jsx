b8fb94091ecee00f67ae2ccef30481cfdab0170f
alexdavies440 <alexdavies440@gmail.com>
2024-09-21 18:57:45 -0500

    Cart now being populated with chosen items, need to figure out how to make remove item button work

--- grocery-project/src/components/GroceryList.jsx
+++ grocery-project/src/components/GroceryList.jsx
@@ -4,21 +4,47 @@ import { useState } from "react";
 
 
 export default function GroceryList () {
+    
     const [subtotal, setSubtotal] = useState(0);
+    const [cart, setCart] = useState([]);
+    function addItem(item) {
+            setSubtotal(subtotal + item.price);
+            setCart(cart => [...cart, `${item.name} - ${item.price}`]);
+            console.log(cart);
+    
+    }
     
     return (
         <div>
             <h3>Pick your groceries: </h3>
             {groceryData.map((items, id) => {
+                
                 return (
                     <ul>
                         <label key={id}>
-                            <button id="addButton" onClick={() => setSubtotal(subtotal + items.price)}>+</button> ${items.price} - {items.name}
+                            <button id="addButton" onClick={() => addItem(items)}>+</button> ${items.price} - {items.name}
                         </label>
                     </ul>
                 );
             })}
-            Subtotal: ${subtotal}
+            
+            <div>Cart:</div>
+            <div>
+                {cart.map((line, id) => {
+                return (
+                    <div>
+                        <ul>
+                            <li key={id}>
+                            <button >-</button> {line} 
+                            </li>
+                        </ul>
+                    </div>
+
+                );
+            })}
+            </div>
+            <div>Subtotal: ${subtotal}</div>
+            
            </div>
            );
         }
