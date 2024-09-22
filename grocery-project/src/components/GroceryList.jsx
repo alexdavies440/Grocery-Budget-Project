84a95a3586bccf96a3298b904663124b3a580580
alexdavies440 <alexdavies440@gmail.com>
2024-09-22 18:05:36 -0500

    Making some progress

--- grocery-project/src/components/GroceryList.jsx
+++ grocery-project/src/components/GroceryList.jsx
@@ -1,63 +1,49 @@
 import groceryData from "./groceryData";
 import { useState } from "react";
+import { useReducer } from "react";
 
 
 
 export default function GroceryList () {
     
-    const [subtotal, setSubtotal] = useState(0);
+    // const [subtotal, setSubtotal] = useState(0);
     const [total, setTotal] = useState(0);
-    const [cart, setCart] = useState([]);
-    function addItem(item) {
-            setSubtotal(subtotal + item.price);
-            setTotal(total + item.price + (item.price * item.taxRate))
-            setCart(cart => [...cart, ` $${item.price} - ${item.name}`]);
-        
-    
+    const [checked, setChecked] = useState(
+        new Array(groceryData.length).fill(false)
+      );
+
+    function handleChange(position) {
+        const updatedCheckedStatus = checked.map((object, index) =>
+            index === position ? !object : object
+        );
+        setChecked(updatedCheckedStatus);
+
+        const totalPrice = updatedCheckedStatus.reduce(
+            (sum, currentState, index) => {
+                if (currentState === true) {
+                    return sum + groceryData[index].price;
+                }
+                return sum;
+            }, 0
+        );
+        setTotal(totalPrice);
     }
-    
-    function removeItem() {
-       
-}
-    // const removeItem = (index, item) => {
-    //     setCart(cart => cart.splice(index));
-    //     setSubtotal(subtotal - item.price);
-    // }
     return (
         <div>
             <h3>Pick your groceries: </h3>
-            {groceryData.map((items, id) => {
+            {groceryData.map((item, id) => {
                 
                 return (
-                    <ul>
-                        <label key={id}><input type="checkbox"  /><input type="number" name="quantity" defaultValue={1} size={1}/> ${items.price} - {items.name} </label>
-                        
-                        {/* <label key={id}>
-                            <button id="addButton" onClick={() => addItem(items)}>Add</button> ${items.price} - {items.name}
-                        </label> */}
+                    <ul key={id}>
+                        <label ><input type="checkbox" checked={checked[id]} onChange={() => handleChange(id)} name="checkbox" id={id} /><input type="number" name="quantity" defaultValue={1} size={1}/> ${item.price} - {item.name} </label>
                     </ul>
                 );
             })}
-            
-            
+       
+            {/* <div>Subtotal: ${Math.round(subtotal * 100)/100}</div> */}
+            <div>Total: ${Math.round(total * 100)/100}</div>
             <div>
-                {cart.map((lineItem, id) => {
-                return (
-                    <div>
-                        <ul>
-                            <li key={id}>
-                            <button onClick={removeItem(lineItem)}>Remove</button> {lineItem} 
-                            </li>
-                        </ul>
-                    </div>
-
-                );
-            })}
-            </div>
-            <div>Subtotal: ${Math.round(subtotal * 100)/100}</div>
-            <div id="total">Total: ${Math.round(total * 100)/100}</div>
-            <div>
-            <label>
+            <label> {console.log(checked)}
                 Max Budget: $<input type="number" id="max" defaultValue={150} size={3}/>
             </label>
             </div>
