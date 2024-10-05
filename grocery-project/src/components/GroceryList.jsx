4606b1be6f15b148d644c46d011031a5d8e49123
alexdavies440 <alexdavies440@gmail.com>
2024-10-04 22:16:28 -0500

    Checkpoint

--- grocery-project/src/components/GroceryList.jsx
+++ grocery-project/src/components/GroceryList.jsx
@@ -8,33 +8,38 @@ import { useId } from "react";
 export default function GroceryList () {
     const currentCart = [];
     let sum;
-    
-    
-    const [subtotal, setSubtotal] = useState(0);
+    let subtotal;
+    //const [count, setCount] = useState(0)
+    //const [subtotal, setSubtotal] = useState(0);
     // const [total, setTotal] = useState(0);
     // const [cart, setCart] = useState([]);
-    function getSubtotal() {
+    function getSubtotal(arr) {
         sum = 0;
-        for (let i = 0; i < currentCart.length; i++) {
-            sum += currentCart[i].price;
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].quantity = 1;
+            sum += arr[i].price * arr[i].quantity;
         }
         return sum;
     }
     
-    function handleChange(id) {
+    function handleChange(index) {
         
-        if (!currentCart.includes(groceryData[id])) {
-            currentCart.push(groceryData[id]);
-            groceryData[id].quantity = 1;
-        }
-        else if (currentCart.includes(groceryData[id])) {
-            currentCart.splice(currentCart.indexOf(groceryData[id]), 1);
-            groceryData[id].quantity = 0;        }
+        if (!currentCart.includes(groceryData[index])) {
+            currentCart.push(groceryData[index]);
+            //currentCart[idex].quantity = 1;
 
-        getSubtotal;
+        }
+        else if (currentCart.includes(groceryData[index])) {
+            currentCart.splice(currentCart.indexOf(groceryData[index]), 1);
+            //currentCart[idex].quantity = 0;
+        }
+        
+        getSubtotal(currentCart);
        //console.log(sum);
+       subtotal = getSubtotal(currentCart);
        console.log(currentCart);
-       console.log(getSubtotal())
+       console.log(subtotal);
+       
     }
     
 
@@ -47,15 +52,16 @@ export default function GroceryList () {
                 return (
                     <ul key={id}>
                         <label ><input type="checkbox" onChange={() =>handleChange(id)} id={id} /></label>
-                        <label><input name="count" type="number" defaultValue={item.quantity} size={1}/> ${item.price} - {item.name}</label>
+                        <label><input name="count" type="number" defaultValue={groceryData[id].quantity} size={1}/> ${item.price} - {item.name}</label>
                         
                     </ul>
+                    
                 );
             })}
 
-            <div>Cart: </div>
+            <div>Cart: {}</div>
        
-            <div>Subtotal: ${getSubtotal()}</div>
+            <div>Subtotal: ${subtotal}</div>
             {/* <div>Total: ${Math.round(total * 100)/100}</div> */}
             
             <div>
